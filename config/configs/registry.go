3a589b163614c7019d96ca73ba941989ae395123
Grezle <4310551+grezle@users.noreply.github.com>
2025-09-09 11:59:22 -0700

    fix: ensure v0.0.3 corresponds with 0.0.3 config (#275)
    <!-- 
        🚨 ATTENTION! 🚨 
        
    This PR template is REQUIRED. PRs not following this format will be
    closed without review.
        
        Requirements:
    - PR title must follow commit conventions:
    https://www.conventionalcommits.org/en/v1.0.0/
    - Label your PR with the correct type (e.g., 🐛 Bug, ✨ Enhancement, 🧪
    Test, etc.)
        - Provide clear and specific details in each section
    -->
    
    **Motivation:**
    <!--
    Explain here the context, and why you're making that change. What is the
    problem you're trying to solve.
    -->
    As a devkit user, I want v0.0.3 to be copied to my project cleanly on
    `devkit avs create ...`
    
    **Modifications:**
    <!--
    Describe the modifications you've done from a high level. What are the
    key technical decisions and why were they made?
    -->
    - Ensures we set the correct config against version number
    
    **Result:**
    <!--
    *After your change, what will change.
    -->
    - Copies over the correct config

--- config/configs/registry.go
+++ config/configs/registry.go
@@ -31,7 +31,7 @@ var v0_0_3_default []byte
 var ConfigYamls = map[string][]byte{
 	"0.0.1": v0_0_1_default,
 	"0.0.2": v0_0_2_default,
-	"0.0.3": v0_0_2_default,
+	"0.0.3": v0_0_3_default,
 }
 
 // Map of sequential migrations
